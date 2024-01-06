b1aa31c2a7569f7966df71d397d21a38fa7bbbd9
BB223 <79644542+BB223@users.noreply.github.com>
2024-01-06 22:57:07 +0100

    missed undotree

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,4 +1,14 @@
 return {
     "ThePrimeagen/harpoon",
     dependencies = { 'nvim-lua/plenary.nvim' },
+    lazy = false,
+    config = function()
+        vim.keymap.set("n", "<leader>a", require("harpoon.mark").add_file)
+        vim.keymap.set("n", "<C-e>", require("harpoon.ui").toggle_quick_menu)
+
+        vim.keymap.set("n", "<C-h>", function() require("harpoon.ui").nav_file(1) end)
+        vim.keymap.set("n", "<C-t>", function() require("harpoon.ui").nav_file(2) end)
+        vim.keymap.set("n", "<C-n>", function() require("harpoon.ui").nav_file(3) end)
+        vim.keymap.set("n", "<C-s>", function() require("harpoon.ui").nav_file(4) end)
+    end,
 }
---
+++ lua/plugins/undotree.lua
@@ -0,0 +1,6 @@
+return {
+    "mbbill/undotree",
+    keys ={
+        vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+    }
+}
