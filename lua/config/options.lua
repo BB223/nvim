f0484065dc1b6fec2dea9ecda8cf466c72b660e7
BB223 <79644542+BB223@users.noreply.github.com>
2026-04-07 19:29:45 +0200

    add gpg

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -76,3 +76,7 @@ vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_node_provider = 0
+
+
+vim.api.nvim_create_user_command('LspInfo', 'checkhealth vim.lsp', {})
+vim.api.nvim_create_user_command('LspLog', 'tabedit ' .. vim.lsp.log.get_filename(), {})
---
+++ lua/plugins/gpg.lua
@@ -0,0 +1,6 @@
+return {
+  'jamessan/vim-gnupg',
+  config = function()
+    vim.g.GPGPreferSign = 1
+  end
+}
